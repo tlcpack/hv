5443da0b626ce0125e5ffe5ee346ae8b64247864
Taylor Cooke <47223694+tlcpack@users.noreply.github.com>
2021-10-21 12:02:52 -0400

    Create ranker.js

---
+++ ranker.js
@@ -0,0 +1,22 @@
+let entities = []
+
+function Entity(p, o, v) {
+	this.p = p;
+  this.o = o;
+  this.v = v;
+}
+
+function ranker(str) {
+	var section = str.split('|')
+ 
+  for (let i = 0; i < section.length; i++ ) {
+  	
+    var total = section[i].split(",")
+    
+    entities.push(new Entity(total[0], total[1], total[2]))
+  }
+  
+  entities.sort((a,b) =>  a.p.localeCompare(b.p) || b.v - a.v)
+  
+  return entities
+}
